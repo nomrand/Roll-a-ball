205ee180f95d0bcac55d0b635fc370587c602257
nomrand <42882840+nomrand@users.noreply.github.com>
2019-07-13 15:58:17 +0700

    5.3

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform target; //Referencetotarget 
+    private Vector3 offset; //relativedistance 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //Getrelativedistancebetween“Player”and“MainCamera”
+        offset = GetComponent<Transform>().position - target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Transform>().position = target.position;
-
+        GetComponent<Transform>().position = target.position + offset;
     }
 }
