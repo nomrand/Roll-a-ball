3a63c39a603124b72484b9f1d35980bfa8732732
nomrand <42882840+nomrand@users.noreply.github.com>
2019-07-13 20:22:11 +0700

    chapter9
    AAAAAAAAA

---
+++ Assets/Scripts/DangerWall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour
+{
+    //Thecall-backofcontacttrigger
+    void OnCollisionEnter(Collision hit)
+    {
+        //WhenPlayercontact“DangerWall”
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            //Getsceneindex 
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            //Reloadscene
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
