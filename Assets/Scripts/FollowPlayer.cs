aa5182aff3252f1b6be140b501cad086c1ce2923
nomrand <42882840+nomrand@users.noreply.github.com>
2019-07-13 15:49:08 +0700

    5.2

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target; //Referencetotarget 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().position = target.position;
+
+    }
+}
